b5d5f0cc2295c747770b79106b7b6d38edc120e0
Shahood Hussain <33638019+ShahoodHussain77@users.noreply.github.com>
2018-11-12 13:22:43 +0500

    Update routes.js

--- route/routes.js
+++ route/routes.js
@@ -21,8 +21,9 @@ module.exports = (app) => {
     // get all products
     app.get('/api/getAll/plantsInfo', productControler.getAllProduct)
     
-
+    // delete single record
     app.post('/api/delete/singleRecord' , productControler.deleteRecord)
 
-
+    // get History 
+    app.post('/api/getSingle/RecordHistory', productControler.getHistory)
 }
